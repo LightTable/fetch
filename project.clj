72bd7911fde7ed8fd1e8381a948e60b912f526cc
Jeroen van Dijk <jeroentjevandijk@gmail.com>
2012-04-26 15:11:24 +0300

    Small typo fix to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject fetch "0.1.0-alpha2"
-  :description "A ClojureScript and Noir library to make client-server intaction painless."
+  :description "A ClojureScript and Noir library to make client-server interaction painless."
   :dependencies [[clojure "1.3.0"]
                  [noir "1.2.2"]])
