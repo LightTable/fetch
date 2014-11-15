bb532e89cea29c89e4e3afcda2346346d5be70ed
Gabriel Horner <gabriel.horner@gmail.com>
2014-11-15 17:21:41 -0500

    Bump to 0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.2.0
+
+* Remove fetch.util since it's unused and causes compiler warnings
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fetch "0.1.1"
+(defproject fetch "0.2.0"
   :description "A ClojureScript and Noir library to make client-server interaction painless."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [noir "1.3.0"]])
